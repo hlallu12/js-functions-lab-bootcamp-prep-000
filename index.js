d19743bb156c263b9fe95f5a95466e37a918239d
hansalallu@gmail.com <hansalallu@gmail.com>
2018-08-14 21:23:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function happyHolidays(string){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 } 
-function happyHolidayTo(holiday)(name){
+function happyHolidayTo(name){
   return `Happy ${holiday}, ${name}!`
 }
